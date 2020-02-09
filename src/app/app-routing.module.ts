dafbe62ff768f54fda02c5aed68df0279e8a8668
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-02-09 20:18:47 +0000

    Added an admin page with canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './pages/public/login-page/login-page.component';
 import { HomePageComponent } from './pages/public/home-page/home-page.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
-  { path: 'login', component: LoginPageComponent }
+  { path: 'login', component: LoginPageComponent },
+  { path: 'admin', canActivate:[ AuthGuard ], component: LoginPageComponent }
 ];
 
 @NgModule({
