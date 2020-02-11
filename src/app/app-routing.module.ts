70fc4238f571f5801831e7391d65338449af111c
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-02-11 15:54:34 +0000

    Fixed routing for dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { LoginPageComponent } from './pages/public/login-page/login-page.component';
 import { HomePageComponent } from './pages/public/home-page/home-page.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+
 import { AuthGuard } from './guards/auth.guard';
 
 
@@ -9,7 +12,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: 'admin', canActivate:[ AuthGuard ], component: LoginPageComponent }
+  { path: 'admin', canActivate:[ AuthGuard ], component: DashboardComponent}
 ];
 
 @NgModule({
