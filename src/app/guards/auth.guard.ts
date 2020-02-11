0c00718dcdfe63aaa8e662baa3958dbb986e29a2
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-02-11 17:17:34 +0000

    Crearted auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -7,13 +7,15 @@ import { AuthService } from '../services/auth/auth.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private auth: AuthService, private router: Router){
+  constructor(private auth: AuthService, private router: Router) {
 
   }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      if (this.auth.getCurrentUser()) return true;
+      this.router.navigate(['/login']);
+      return false;
   }
-  
 }
