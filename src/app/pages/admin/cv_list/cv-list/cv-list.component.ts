b0606b63d96ac6c79ae0c39a0158898319cc3e9f
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-02-12 15:32:24 +0000

    Added CV list component

---
+++ src/app/pages/admin/cv_list/cv-list/cv-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cv-list',
+  templateUrl: './cv-list.component.html',
+  styleUrls: ['./cv-list.component.scss']
+})
+export class CvListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
