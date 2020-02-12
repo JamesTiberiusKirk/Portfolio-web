e8e2f69fdf96f225182d9c2a3b25e07517095e9f
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-02-12 09:33:52 +0000

    Added a logout button to admin dashboard

--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth:AuthService) { }
 
   ngOnInit() {
+  }
+
+  logoutBtn(){
+    this.auth.logout();
   }
 
 }
